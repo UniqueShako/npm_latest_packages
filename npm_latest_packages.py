2b36d8b27df088f8f8c2cdd4aa87f00ff0615459
Taylor <41836534+UniqueShako@users.noreply.github.com>
2022-08-05 10:16:43 -0600

    Update npm_latest_packages.py

--- npm_latest_packages.py
+++ npm_latest_packages.py
@@ -10,7 +10,7 @@ from os.path import exists
 #This program queries the libraries.io API for new and newly updated NPM packages for today. It then takes the package name, date published, and version number and adds them to a CSV file
 #in the current directory. If the CSV file does not exist it will be created automatically. Finally it will remove duplicate entries from the CSV file and output the _final CSV file.
 
-#To run this program yourself you will need a libraries.io API key, which can be obtained by signing up for a freee libraries.io acocunt and checking your account settings page.
+#To run this program yourself you will need a libraries.io API key, which can be obtained by signing up for a free libraries.io account and checking your account settings page.
 #This program depends on the "requests" library which you may need to install using "pip install requests"
 
 #This may take several minutes to run depending on how much load is being put on the libraries.io servers. It will handle errors and wait 2 minutes between each error code returned from the server.
