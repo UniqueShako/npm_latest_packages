6fa184b68aa7e565615f33165a07846e826da0ab
Taylor <41836534+UniqueShako@users.noreply.github.com>
2022-09-12 15:11:45 -0600

    added ossgadget download functionality

--- npm_latest_packages.py
+++ npm_latest_packages.py
@@ -10,18 +10,21 @@ from os.path import exists
 #This program queries the libraries.io API for new and newly updated NPM packages for today. It then takes the package name, date published, and version number and adds them to a CSV file
 #in the current directory. If the CSV file does not exist it will be created automatically. Finally it will remove duplicate entries from the CSV file and output the _final CSV file.
 
-#To run this program yourself you will need a libraries.io API key, which can be obtained by signing up for a free libraries.io account and checking your account settings page.
+#To run this program yourself you will need a libraries.io API key, which can be obtained by signing up for a freee libraries.io acocunt and checking your account settings page.
 #This program depends on the "requests" library which you may need to install using "pip install requests"
 
 #This may take several minutes to run depending on how much load is being put on the libraries.io servers. It will handle errors and wait 2 minutes between each error code returned from the server.
 #If it encounters 10 error messages over the course of running, the program will clean up duplicates and end.
 
+#IN ORDER FOR THE DOWNLOADER TO WORK YOU MUST CREATE THE "packages_dl" directory in the directory you are running the scraper from
 
 
-api_key = 'yourapikeyhere' #PUT YOUR API KEY HERE SURROUNDED BY QUOTES eg. 'thisisyourapikey'
+
+api_key = '7806edf5c32149673796ccc311180404' #PUT YOUR API KEY HERE SURROUNDED BY QUOTES eg. 'thisisyourapikey'
 counter = 0 #used to increase page count
 failcount = 0 #keep track of failures
 keepgoing = True #looper, set set to false if date does not equal today
+ossgadgetpath = '.\ossgadget' #path to ossgadget folder. By default it looks at current directory
 today = str(datetime.today()).split(" ") #gets today's date and splits it at the time
 if exists('.\packages{}.csv'.format(today[0])): #checks if the packages have already been scraped today, if it has then it deletes the old file and starts fresh. need to build some logic in so that it only scrapes new updates instead of all updates for the day.
     os.remove('.\packages{}.csv'.format(today[0]))
@@ -33,7 +36,6 @@ def remove_duplicates(): #iterates through csv file and outputs _final csv file 
     listLines = []
     for line in inFile:
         if line in listLines:
-            print("Skipped line {}".format(line))
             continue
         else:
             outFile.write(line)
@@ -43,7 +45,7 @@ def remove_duplicates(): #iterates through csv file and outputs _final csv file 
 
 
 while keepgoing:
-    response = requests.get("https://libraries.io/api/search?order=desc&page={}&per_page=100&platforms=npm&sort=latest_release_published_at&api_key={}".format(counter+1, api_key))
+    response = requests.get("https://libraries.io/api/search?order=desc&page={}&per_page=99&platforms=npm&sort=latest_release_published_at&api_key={}".format(counter+1, api_key))
     print("Scraping page {}".format(counter+1)) #queries the api for most recent packages with 100 per page. Iterates through pages using counter. Prints debug info
     print("HTTP status code: {}".format(response.status_code))
     if str(response.status_code) == '200':
@@ -60,7 +62,10 @@ while keepgoing:
         continue
 
     for package in range(len(raw)): #iterates through each package that was returned on the page (100)
-        name = raw[package]['name']
+        if '@' in raw[package]['name']:
+            name = raw[package]['name'].replace('@', '%40').replace('/', '%2F')
+        else:
+            name = raw[package]['name']
         date = raw[package]['latest_release_published_at']
         version_number = raw[package]['latest_release_number'] #assign variables and split dates so they are in the same format
         header = ['name', 'date', 'version']
@@ -74,6 +79,7 @@ while keepgoing:
                 writer.writerow(header)
             if pubdate[0] == today[0]: #check if package was published today, if so, write it to the csv. Uses the [0] index because the date is split into a list [date, time] and we only need the first element
                 writer.writerow(final_data)
+                os.system('cmd /k "{}\oss-download -e -x ./packages_dl pkg:npm/{} & exit"'.format(ossgadgetpath, name)) #downloads package and puts it into "packages_dl" which you will need to create manually
             elif pubdate[0] != today[0]: #if package was not published today, break the loop and finish.
                 print("All packages published today {} have been added to the packages.csv file".format(today[0]))
                 remove_duplicates()
